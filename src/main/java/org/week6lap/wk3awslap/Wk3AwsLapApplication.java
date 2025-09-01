ca528d3a0fe533d660def78fe00c9e7319ce1f18
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-09-01 17:53:05 +0000

    initial commit

---
+++ src/main/java/org/week6lap/wk3awslap/Wk3AwsLapApplication.java
@@ -0,0 +1,13 @@
+package org.week6lap.wk3awslap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wk3AwsLapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wk3AwsLapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/week6lap/wk3awslap/Wk3AwsLapApplicationTests.java
@@ -0,0 +1,13 @@
+package org.week6lap.wk3awslap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wk3AwsLapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
