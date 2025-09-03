888ff774ca585a4f06e2e0bc8cebee6573c4675c
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-09-03 17:55:14 +0000

    feat add ui to server and serve via hello controller

--- src/main/java/org/week6lap/wk3awslap/controller/HelloController.java
+++ src/main/java/org/week6lap/wk3awslap/controller/HelloController.java
@@ -1,12 +1,21 @@
 package org.week6lap.wk3awslap.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
-    @GetMapping()
-    public String hello() {
-        return "Hello World";
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("message", "Hello from Spring Boot!");
+        return "index";
+    }
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("greeting", "Hello, world! 🚀");
+        return "hello";
     }
 }
